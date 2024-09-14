4526b81b5fe244978282685cf9b2fddb5346fb96
imranrasheedsc <107059911+imranrasheedsc@users.noreply.github.com>
2024-09-13 18:23:11 -0700

    Update NeighborhoodRunner.java

--- NeighborhoodRunner.java
+++ NeighborhoodRunner.java
@@ -4,7 +4,9 @@ public class NeighborhoodRunner {
   public static void main(String[] args) {
     MuralPainter alex = new MuralPainter();
     WatermelonPainter joe = new WatermelonPainter();
+    // Paints the background and adds 256 paint to the painters bucket because it is in a 16x16 grid format.
     alex.paintBackground("white",256);
+    // Runs the paint watermelon function, painting the entire watermelon.
     joe.paintWatermelon();
   }
 }
